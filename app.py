88aa6c24d3ca129a8ff69695cfca994a9cfce9c0
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-11 11:17:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,10 +13,16 @@ def current_gold_price():
     s_price = article.strong.text
     price = int((s_price.split()[1]).replace(',',''))
     return price
-prev = 0
+
 i = 0
-while i<10000000:
-    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}".format(i))
+start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+cur_val = int(current_gold_price())
+end = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+sstart = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={} {}".format("price", (str(start-end)))
+eend = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={} {}".format("tele", (str(sstart-eend)))
+
 #     cur_val = int(current_gold_price())
 #     if prev != cur_val:
 #         start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
