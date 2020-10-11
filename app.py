abc0e56b0777e29d88b7e4d2711bbafaa3f67e1f
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-11 10:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ def current_gold_price():
 prev = 0
 i = 0
 while i<10000000:
-    cur_val = int(current_gold_price())
-    if prev != cur_val:
-        start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-        requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={} : {} || {}".format(str(i), str(prev), str(cur_val)))
-    prev = cur_val
+    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}".format(i))
+#     cur_val = int(current_gold_price())
+#     if prev != cur_val:
+#         start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+#         requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}".format(i))
+#     prev = cur_val
     i+=1
