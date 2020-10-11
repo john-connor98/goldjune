f3fb06ffd28fee03bac2c0d0436fb7ce39a4ea4a
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-11 21:15:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def current_gold_price():
     s_price = article.strong.text
     price = int((s_price.split()[1]).replace(',',''))
     return price
-def app():
-    main()
+# def app():
+#     main()
 def main():
     for i in range(3):
         start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
@@ -23,9 +23,9 @@ def main():
         requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={} {} {} {}".format(i, str(cur_val), str(start), str(end)))
         sleep(5)
     
-
-if __name__ == "__main__":
-    app()
+main()
+# if __name__ == "__main__":
+#     app()
 
 #     cur_val = int(current_gold_price())
 #     if prev != cur_val:
