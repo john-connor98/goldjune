23e1c0556c681cad6876af7458b4bf62c7026c7b
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-13 07:41:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,19 @@ def current_gold_price():
     s_price = article.strong.text
     price = int((s_price.split()[1]).replace(',',''))
     return price
-# def app():
-#     main()
-def main():
-    for i in range(3):
-        start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-        cur_val = int(current_gold_price())
-        end = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-        requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={} {} {} {}".format(i, str(cur_val), str(start), str(end)))
-        sleep(5)
-    
-main()
-# if __name__ == "__main__":
-#     app()
 
-#     cur_val = int(current_gold_price())
-#     if prev != cur_val:
-#         start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-#         requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}".format(i))
-#     prev = cur_val
+def job():
+    start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+    cur_val = int(current_gold_price())
+    end = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}| {}| {}| {}".format(i, str(cur_val), str(start), str(end)))
+        
+def main():
+    schedule.every(10).minutes.do(job)
+    
+    while True:
+    schedule.run_pending()
+    time.sleep(1)
+    
+if __name__ == "__main__":
+    main()
