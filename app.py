7129aeb3e0fa6eba6132197c079043d6dd3de021
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-13 08:22:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ def current_gold_price():
     price = int((s_price.split()[1]).replace(',',''))
     return price
 
-def job():
-    start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-    cur_val = int(current_gold_price())
-    end = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}| {}| {}| {}".format(i, str(cur_val), str(start), str(end)))
+# def job():
+#     start = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+#     cur_val = int(current_gold_price())
+#     end = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+#     requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}| {}| {}| {}".format(i, str(cur_val), str(start), str(end)))
         
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    job()
+    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text=chetan")
 
 sched.start()
