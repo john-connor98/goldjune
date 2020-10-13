9bb2215b26facd6314207c017ed18d59c56db712
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-13 09:59:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 from time import gmtime, strftime, sleep
 import schedule
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+prev = 0
 def current_gold_price():
     r = requests.get('https://www.goodreturns.in/gold-rates/').text
     soup = BeautifulSoup(r, "html.parser")
@@ -16,14 +16,17 @@ def current_gold_price():
     price = int((s_price.split()[1]).replace(',',''))
     return price
 
-def job():
-    cur_val = int(current_gold_price())
-    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text=Price :- {}".format(str(cur_val)))
+# def job():
+#     cur_val = int(current_gold_price())
+#     requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text=Price :- prev = {}: curr = {}".format(str(prev), str(cur_val)))
         
 sched = BlockingScheduler()
 
+
 @sched.scheduled_job('interval', minutes=2)
 def timed_job():
-    job()
+    cur_val = int(current_gold_price())
+    requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text=Price :- prev = {}: curr = {}".format(str(prev), str(cur_val)))
+     
 
 sched.start()
