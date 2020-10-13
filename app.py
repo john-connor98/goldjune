2dac4e62c2bda55ddfeecf7c3afc067d52bf0e47
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-13 08:15:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import pandas as pd
 from time import gmtime, strftime, sleep
 import schedule
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 def current_gold_price():
     r = requests.get('https://www.goodreturns.in/gold-rates/').text
@@ -21,12 +22,10 @@ def job():
     end = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
     requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text={}| {}| {}| {}".format(i, str(cur_val), str(start), str(end)))
         
-def main():
-    schedule.every(10).minutes.do(job)
-    
-    while True:
-        schedule.run_pending()
-        sleep(1)
-    
-if __name__ == "__main__":
-    main()
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    job()
+
+sched.start()
