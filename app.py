b56b594d5afa1de9abd0406a414230cc84215711
john-connor98 <68577331+john-connor98@users.noreply.github.com>
2020-10-15 10:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def timed_job():
     last_price = int(fetchprice())
     if cur_val != last_price:
         requests.get("https://api.telegram.org/bot1340927566:AAHzy54vtOJcqB2OKO5Qgo5vHzLxvNYdkRY/sendMessage?chat_id=985062789&text=Price :- last = {} : curr = {}".format(str(last_price), str(cur_val))) 
-        updateprice()
+        updateprice(cur_val)
 sched.start()
